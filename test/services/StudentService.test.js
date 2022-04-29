0d15369a8e97bf1316004bddfd38e66b567d3478
Raul Quintero <raulquintero13@gmail.com>
2022-04-28 20:26:35 -0700

    adding StudentModel & supertest for the route /, & fixing some  tests

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,34 +1,41 @@
 
 const StudentService = require("./../../lib/services/StudentService.js");
-const Database = require("../../lib/utils/Database");
+const StudentModel = require('../../lib/models/StudentModel')
+// const Database = require('../../lib/utils/Database');
 const db = "./test/students.test.json";
 const dbEmpty = "./test/studentsEmpty.test.json";
 
-describe("test for StudentService Class", ()=> {
 
+
+describe("test for StudentService Class", ()=> {
+    
     // endpoint /v1/students
     test("test for getStudents()", ()=> {
-        let students = Database.studentsAll(db);
+        const StudentClass = new StudentModel(db);
+        let students = StudentClass.getAll();
         students = StudentService.getStudents(students);
         expect(students.length).toBe(51);
     });
 
     test("test for getStudents() with DB empty", ()=> {
-        let students = Database.studentsAll(dbEmpty);
+        const StudentClass = new StudentModel(dbEmpty);
+        let students = StudentClass.getAll();
         students = StudentService.getStudents(students);
         expect(students.length).toBe(0);
     });
 
     //endpoint /v1/students/with-certification/show/email
     test("for getStudentsWithCertification", ()=> {
-        let students = Database.studentsAll(db);
+        const StudentClass = new StudentModel(db);
+        let students = StudentClass.getAll();
         students = StudentService.getStudentsWithCertification(students);
         expect(students.length).toBe(29);
     })
 
     //endpoint /v1/students/score-greater-than/show/email
     test("for getStudentsWithCertification", ()=> {
-        let students = Database.studentsAll(db);
+        const StudentClass = new StudentModel(db);
+        let students = StudentClass.getAll();
         students = StudentService.getStudentsScoreGreaterThan(students);
         expect(students.length).toBe(27);
     })
